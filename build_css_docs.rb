54e50cfe52a9242682ba46dc33ff20a742441395
Tanner Welsh <tannerwelsh@gmail.com>
2020-12-14 17:01:34 -0700

    Get attribute parser working, match folder structure

--- build_css_docs.rb
+++ build_css_docs.rb
@@ -1,6 +1,10 @@
 require "crass"
 require "pp"
 
+SOURCE_DIR = File.join(__dir__, 'css')
+SOURCE_FILES = Dir.glob(File.join("**", "*.css"), base: SOURCE_DIR)
+DESTINATION_DIR = File.join(__dir__, 'css_docs')
+
 def extract_docs_from_css(path, tree)
   prev_comment = nil
 
@@ -21,30 +25,49 @@ def extract_docs_from_css(path, tree)
 
       doc['source_file'] = path
       doc['content'] = prev_comment
+      doc_attrs = parse_attributes(doc['content'])
+      doc['attrs'] = doc_attrs
 
-      write_to_collection(doc)
+      write_to_collection(doc, path)
 
       prev_comment = nil
     end
   end
 end
 
-def write_to_collection(doc)
-  destination_dir = File.join(__dir__, 'css_docs', '_example')
+def parse_attributes(docstring)
+  # TODO: support multiline attributes (like @description, @example)
+  #       (see button.css)
+  attrs = {}
+
+  lines = docstring.lines
+  lines.each do |line|
+    if line.lstrip[0] == '@'
+      attr_name, *attr_value = line.split ' '
+      attrs[attr_name[1..-1]] = attr_value.join ' '
+    end
+  end
+
+  attrs
+end
+
+def write_to_collection(doc, filepath)
+  # pull out the tokens from /savile/css/tokens/colors.css
+  file_folder = File.basename(File.dirname(filepath))
+  docs_folder = File.join(DESTINATION_DIR, file_folder)
+  Dir.mkdir docs_folder unless Dir.exist? docs_folder
   filename = doc['title'] + '.md'
 
-  File.write(File.join(destination_dir, filename), doc['content'])
+  # TODO: add doc['attrs'] as head matter (YAML) to the .md file
+  File.write(File.join(docs_folder, filename), doc['content'])
 end
 
 def clean_comment(comment_text)
   comment_text.lines.map { |l| l.sub(/^\s*\*\s?/, '') }.join
 end
 
-source_dir = File.join(__dir__, 'css')
-source_files = Dir.glob(File.join("**", "*.css"), base: source_dir)
-
-source_files.each do |file|
-  filepath = File.join(source_dir, file)
+SOURCE_FILES.each do |file|
+  filepath = File.join(SOURCE_DIR, file)
   css = File.read(filepath)
   tree = ::Crass.parse(css, :preserve_comments => true)
 
